505e18664703a4c2f778d421b97346831fb9947e
Kael <i@kael.me>
2016-05-16 14:15:58 +0800

    1.2.1: fixes babel

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+export PATH=$PATH:./node_modules/.bin/
+export IGNORE_TEST_WIN32=
 
-type babel && babel src --out-dir lib || echo 'warn: babel failed'
+type babel && babel src --out-dir lib --presets es2015 || echo 'warn: babel failed'
